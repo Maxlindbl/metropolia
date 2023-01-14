16d34f8b8b2f1e55aeb78044cd9f4ccbc6b62468
maxlindblad <max.lindblad@metropolia.fi>
2023-01-14 16:27:48 +0200

    koulu

--- koulu/ohjelmisto 1/moduuli 4/tehtävä  5.py
+++ koulu/ohjelmisto 1/moduuli 4/tehtävä  5.py
@@ -2,6 +2,7 @@ i = 1
 while i <= 5:
     if input("käyttäjätunnus: ") == "python" and input("salasana: ") == "rules":
         print("tervetuloa")
+        break
     elif i > 5:
         break
-i = i + 1
+    i = i + 1
