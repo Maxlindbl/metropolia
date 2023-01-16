368c9d46d1b78c555278990e1a9d2e273e1dcfe6
maxlindblad <max.lindblad@metropolia.fi>
2023-01-16 16:02:17 +0200

    koulu

--- koulu/ohjelmisto 1/moduuli 5/tehtävä 3.py
+++ koulu/ohjelmisto 1/moduuli 5/tehtävä 3.py
@@ -2,9 +2,10 @@ number = int(input("anna luku: "))
 
 if number == 1:
     print("luku ei ole alkuluku")
-for i in range(2, number):
-    if number % i == 0:
-        print("luku ei ole alkuluku")
-        break
-else:
-    print("luku on alkuluku")
+elif number > 1:
+    for i in range(2, number):
+        if number % i == 0:
+            print("luku ei ole alkuluku")
+            break
+    else:
+        print("luku on alkuluku")
