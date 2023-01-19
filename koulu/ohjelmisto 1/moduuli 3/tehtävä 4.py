2a0fb4d6121dcccd11b1f962cfea8f75d958d0ea
maxlindblad <max.lindblad@metropolia.fi>
2023-01-19 13:05:48 +0200

    koulu

--- koulu/ohjelmisto 1/moduuli 3/tehtävä 4.py
+++ koulu/ohjelmisto 1/moduuli 3/tehtävä 4.py
@@ -1,6 +1,11 @@
 #kysytään vuotta ja lasketaan onko se karkausvuosi.
 year = int(input("anna vuosiluku: "))
-if year % 4 == 0 or year % 400 == 0:
+if year % 100 == 0:
+    if year % 400 == 0:
+        print("vuosi on karkausvuosi")
+    else:
+        print("vuoi ei ole karkausvuosi")
+elif year % 4 == 0:
     print("vuosi on karkausvuosi")
 else:
     print("vuosi ei ole karkausvuosi")
