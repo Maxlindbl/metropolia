34005e24ff8469cbac72d0fee8b94d1d2ce399c0
maxlindblad <max.lindblad@metropolia.fi>
2023-01-21 18:21:22 +0200

    koulu

---
+++ koulu/ohjelmisto 1/moduuli 8/tehtävä 1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+def airports(icao):
+    sql = "SELECT name, iso_region FROM airport"
+    sql += " WHERE ident='" + icao + "'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+        for rivi in tulos:
+            print(f"kenttä on nimeltään {rivi[0]}. ja se sijaitsee:  {rivi[1]}.")
+    return
+
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='max',
+         password='password',
+         autocommit=True
+         )
+icao = input("Anna lentokentän icao koodi: ")
+airports(icao)
