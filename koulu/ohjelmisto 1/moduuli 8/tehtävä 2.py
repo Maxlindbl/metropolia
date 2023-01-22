fe1806f0818e24fa10755fad4e89b84bd1a9b1c0
maxlindblad <max.lindblad@metropolia.fi>
2023-01-22 10:35:13 +0200

    koulu

---
+++ koulu/ohjelmisto 1/moduuli 8/tehtävä 2.py
@@ -0,0 +1,27 @@
+import mysql.connector
+def airports(country):
+    sql = "SELECT type FROM airport"
+    sql += " WHERE iso_country='" + country + "'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    calc = {i: result.count(i) for i in result}
+    print(calc)
+    return
+
+
+
+
+
+connection = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='max',
+         password='password',
+         autocommit=True
+         )
+
+country = input("anna lentokentän maatunnus: ")
+airports(country)
