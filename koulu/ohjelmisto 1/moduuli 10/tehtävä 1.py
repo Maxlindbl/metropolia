e2f97bfcb8bfcb1bd91640ccd74b427d1150401b
maxlindblad <max.lindblad@metropolia.fi>
2023-01-24 14:07:14 +0200

    koulu

---
+++ koulu/ohjelmisto 1/moduuli 10/tehtävä 1.py
@@ -0,0 +1,30 @@
+class Elevator:
+    def __init__(self,top_floor, bottom_floor, floor):
+        self.top_floor = top_floor
+        self.bottom_floor = bottom_floor
+        self.floor = floor
+#määrittellään pitääkö hissin mennä ylös vai alas
+    def Go_floor(self, go_to):
+        if self.floor < go_to:
+            while self.floor != go_to:
+                self.Go_up()
+        if self.floor > go_to:
+            self.Go_down()
+            while self.floor != go_to:
+                self.Go_down()
+        print("olet perillä")
+#hissi ylös
+    def Go_up(self,):
+        self.floor = self.floor + 1
+        print(self.floor)
+#hissi alas
+    def Go_down(self,):
+        self.floor = self.floor - 1
+        print(self.floor)
+
+
+#kutsutaan hissiä
+elevator = Elevator(9,0,0)
+elevator.Go_floor(7)
+elevator.Go_floor(0)
+
