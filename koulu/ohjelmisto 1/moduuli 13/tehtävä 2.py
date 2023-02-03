7556184d8269aa2b90529de567e078bea843bfd5
maxlindblad <max.lindblad@metropolia.fi>
2023-02-03 14:34:47 +0200

    koulu

---
+++ koulu/ohjelmisto 1/moduuli 13/tehtävä 2.py
@@ -0,0 +1,42 @@
+import mysql.connector
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def get_airport_data(icao):
+    sql = "SELECT name, municipality FROM airport"
+    sql += " WHERE ident='" + icao + "'"
+    #print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    #if kursori.rowcount >0 :
+        #for rivi in tulos:
+            #print(f"kenttä on nimeltään {rivi[0]}. ja se sijaitsee:  {rivi[1]}.")
+
+    return tulos
+
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='max',
+         password='password',
+         autocommit=True
+         )
+
+
+@app.route("/kenttä/<icao>")
+def answer(icao):
+    airport_data = get_airport_data(icao)
+    if airport_data is not None:
+        response = {"ICAO": icao, "Name": airport_data[0][0], "Municipality": airport_data[0][1]}
+        return jsonify(response)
+    else:
+        return jsonify({"error": "No data found for ICAO code " + icao})
+
+if __name__ == "__main__":
+    app.run(use_reloader=True, host = '127.0.0.1', port=3000)
+
+
+
