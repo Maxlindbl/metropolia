0bd25abd5fef3384d89ea07e72532277284a7eef
maxlindblad <max.lindblad@metropolia.fi>
2023-02-03 13:54:57 +0200

    koulu

--- koulu/ohjelmisto 1/moduuli 13/tehtävä 1.py
+++ koulu/ohjelmisto 1/moduuli 13/tehtävä 1.py
@@ -1,2 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+def is_prime(number):
+    if number == 1:
+        return False
+    for i in range(2, number):
+            if number % i == 0:
+                return False
+    return True
+
+@app.route("/alkuluku/<int:number>")
+def prime_check(number):
+    response = {"Number": number, "isPrime": is_prime(number)}
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(use_reloader=True, host = '127.0.0.1', port=3000)
